e49a763744f7cfea2514b7b07eb74dce9d0f7869
Jhonatan <yamidjhonatan@gmail.com>
2022-01-19 21:14:56 -0500

    Commit inicial

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
 import useMoneda from '../hooks/useMoneda'
 import useCriptomoneda from '../hooks/useCriptomoneda'
-import axios, { Axios } from 'axios'
+import axios from 'axios'
 import Error from './Error'
 
 const Boton = styled.input`
